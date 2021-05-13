1894068d42991f52f8b0cc62db57b26cd68328f7
Travis Frazier <travisfrazier@traviss-mbp.lan>
2021-05-13 11:46:14 -0700

    updates to form

---
+++ assets/main.js
@@ -0,0 +1,43 @@
+const element = document.querySelector('.c-form__select');
+const button = document.querySelector('.c-form__button');
+
+//Choice JS --- Custom Select Dropdown for State ----- //
+const choices = new Choices(element, {
+    silent: false,
+    items: [],
+    choices: [],
+    renderChoiceLimit: -1,
+    maxItemCount: -1,
+    addItems: true,
+    addItemFilter: null,
+    removeItems: true,
+    removeItemButton: false,
+    editItems: false,
+    duplicateItemsAllowed: true,
+    delimiter: ',',
+    paste: true,
+    searchEnabled: true,
+    searchChoices: true,
+    searchFloor: 1,
+    searchResultLimit: 4,
+    searchFields: ['label', 'value'],
+    position: 'auto',
+    resetScrollPosition: true,
+    shouldSort: true,
+    shouldSortItems: false,
+    placeholder: true,
+    placeholderValue: null,
+    searchPlaceholderValue: null,
+    prependValue: null,
+    appendValue: null,
+    renderSelectedChoices: 'auto',
+    loadingText: 'Loading...',
+    noResultsText: 'No results found',
+    noChoicesText: 'No choices to choose from',
+    itemSelectText: ''
+  });
+
+  //Form Button Submit ----- //
+  button.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
